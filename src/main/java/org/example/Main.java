b1a560d2e323ea7faa9c8c7dd17d3ff2cae4c88f
LinusAaberg <linus.abergg@gmail.com>
2023-02-08 22:22:02 +0100

    Altered some commits

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,20 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.print(helloWorld());
+        System.out.print(byeWorld());
+        System.out.print(helloLinus());
     }
 
     public static String helloWorld() {
         return "Hello World";
+    }
+
+    public static String helloLinus() {
+        return "Hello Linus";
+    }
+
+    public static String byeWorld() {
+        return "Bye World";
     }
 
     public static String answers(int select) {
